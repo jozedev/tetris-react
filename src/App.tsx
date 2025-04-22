637e0c16f8658f14da478726cac396227863c2bc
José Pecho <jpechob@odybank.com.pe>
2025-04-22 09:19:28 -0500

    Site with ads

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Add from './components/Add'
 import { TetrisGame } from './components/TetrisGame'
 
 function App () {
@@ -7,6 +8,7 @@ function App () {
       <main>
         <h1 className="title">TETRIS</h1>
         <TetrisGame />
+        <Add />
       </main>
       <footer>
         <small>You can check out the code <a href='https://github.com/jozedev/tetris-react' target='_blank' rel="noreferrer">here</a>.</small>
---
+++ src/components/Add.tsx
@@ -0,0 +1,10 @@
+const Add = () => {
+  return (
+    <div className='add'>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5095155460088229"
+      crossOrigin="anonymous"></script>
+    </div>
+  )
+}
+
+export default Add
